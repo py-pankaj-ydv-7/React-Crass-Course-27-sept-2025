346cfd934b0a760440811d91282876c3c43d4379
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-09-30 02:17:21 +0530

    understanding about screens

---
+++ TGReactCrassCourse/myapp/src/Contact.js
@@ -0,0 +1,9 @@
+// Boilerplate Code:
+import React from "react";
+
+function Contact(){
+    return <h1>this is Contact screen</h1>
+
+}
+
+export default Contact;
---
+++ TGReactCrassCourse/myapp/src/screens/About.js
@@ -0,0 +1,9 @@
+// Boilerplate Code:
+import React from "react";
+
+function About(){
+    return <h1>this is about screen</h1>
+
+}
+
+export default About;
