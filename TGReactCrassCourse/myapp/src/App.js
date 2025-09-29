897b6e993203633f1c279d78fb78b62029aa3918
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-09-30 02:17:12 +0530

    understanding about screens

--- TGReactCrassCourse/myapp/src/App.js
+++ TGReactCrassCourse/myapp/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './screens/About';
+import Contact from './Contact';
 
 function App() {
   return (
     <div>
       
       <h1>Hello World</h1>
+      <About/>
+      <Contact/>
     </div>
   );
 }
