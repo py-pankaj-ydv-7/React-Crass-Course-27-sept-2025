0e23cd9565007d1b2b97994cd2405927001533ba
Pankaj Yadav <pankajyadav851685@gmail.com>
2025-09-30 22:47:29 +0530

    mapping component

---
+++ TGReactCrassCourse/myapp/src/utils/data.js
@@ -0,0 +1,28 @@
+let data = [
+    {
+    name: "pankaj",
+    email:"py@gmail.com",
+    age: 19,
+    rollNo: 101,
+  },
+    {
+    name: "summi",
+    email:"sy@gmail.com",
+    age: 19,
+    rollNo: 102,
+  },
+    {
+    name: "jitu",
+    email:"jy@gmail.com",
+    age: 19,
+    rollNo: 103,
+  },
+    {
+    name: "nitin",
+    email:"ny@gmail.com",
+    age: 19,
+    rollNo: 104,
+  },
+];
+
+export default data;
